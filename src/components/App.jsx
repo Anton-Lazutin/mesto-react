a19bad7db7fa5634489dac59b8a9fb65e8e71a23
Anton Lazutin <laa.63@yandex.ru>
2023-08-03 23:45:47 +0300

    changed ImagePopup and README

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import Header from "./Header/Header.jsx";
 import Main from './Main/Main.jsx';
 import Footer from './Footer/Footer.jsx';
 import PopupWithForm from "./PopupWithForm/PopupWithForm.jsx";
-import PopupImage from "./PopupImage/PopupImage.jsx";
+import ImagePopup from "./ImagePopup/ImagePopup.jsx";
 import { useState } from "react";
 
 function App() {
@@ -134,7 +134,7 @@ function App() {
       titleButton = 'Да'
     />
 
-    <PopupImage 
+    <ImagePopup
     card = {selectedCard}
     isOpen = {isImagePopup}
     onClose = {closeAllpopups}
